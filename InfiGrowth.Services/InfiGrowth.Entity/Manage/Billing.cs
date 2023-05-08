e6b7564f487ea7741ad2916b055cc192ea5785cd
smit-skyttus <smit.skyttus@gmail.com>
2023-05-08 12:05:52 +0530

    models added

---
+++ InfiGrowth.Services/InfiGrowth.Entity/Manage/Billing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiGrowth.Entity.Manage
+{
+    public class Billing
+    {
+        public Guid BillingId { get; set; }
+        public Guid Id { get; set; }
+        [ForeignKey("Id")]
+        public virtual Customer Customer { get; set; }
+        public DateTime Date { get; set; }
+        public string PaymentType { get;set; }
+        public double Tax { get; set; } = 500;
+        public double TotalPrice { get; set; }
+    }
+}
---
+++ InfiGrowth.Services/InfiGrowth.Entity/Manage/Booking.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiGrowth.Entity.Manage
+{
+    public class Booking
+    {
+        public Guid BookingId { get; set; }
+        public Guid HotelId { get; set; }
+        [ForeignKey("HotelId")]
+        public virtual Hotel Hotel { get; set; }
+        public Guid BillingId { get; set; }
+        [ForeignKey("BillingId")]
+        public virtual Billing Billing { get; set; }
+        public DateOnly CheckIn { get; set; }
+        public DateOnly CheckOut { get; set;}
+
+    }
+}
