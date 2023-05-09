25fc70b9f62f2d5b537f41b3e5b1faec679f28ed
smit-skyttus <smit.skyttus@gmail.com>
2023-05-09 12:58:00 +0530

    hotel added

---
+++ InfiGrowth.Services/InfiGrowth.Infra/Repository/Interfaces/IHotelRepository.cs
@@ -0,0 +1,14 @@
+﻿using InfiGrowth.Entity.Manage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfiGrowth.Infra.Repository.Interfaces
+{
+    public interface IHotelRepository
+    {
+        Task<Hotel> GetHotelById(Guid HotelId);
+    }
+}
